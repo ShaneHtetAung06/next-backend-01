d48c43a2bff564422e87405c40b25a539d7cc658
Shane Htet Aung <raahulladk@gmail.com>
2026-09-07 15:09:04 +0700

    added auth system

---
+++ app/api/me/route.js
@@ -0,0 +1,43 @@
+//src/app/api/me/route.js 
+
+ 
+
+import { verifyJWT } from "@/lib/auth"; 
+
+import corsHeaders from "@/lib/cors"; 
+
+import { errorResponse } from "@/lib/utils"; 
+
+import { NextResponse } from "next/server"; 
+
+ 
+
+// Handle CORS preflight requests
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
+}
+
+export function GET(request) { 
+
+ const user = verifyJWT(request); 
+
+ if (!user) { 
+
+  return errorResponse("Unauthorized Request", 401); 
+
+ } 
+
+ return NextResponse.json(user, { 
+
+  status: 201, 
+
+  headers: corsHeaders, 
+
+ }); 
+
+} 
+
+ 
